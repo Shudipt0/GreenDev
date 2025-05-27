b2d06fd69a50e3fc73a152daab708532af1493ce
Shudipt0 <royshudipto318@gmail.com>
2025-05-28 00:33:22 +0600

    complete image uploading

--- app/_components/ImageUpload.tsx
+++ app/_components/ImageUpload.tsx
@@ -1,7 +1,22 @@
 "use client";
 import { UploadButton , UploadDropzone } from "@/utils/uploadthing";
+import Image from "next/image";
+import { useState } from "react";
+import {imageRemove} from "../actions/imageRemove"
 
 const ImageUpload = () => {
+  const [imageUrl, setImageUrl] = useState("")
+  const [imageKey, setImageKey] = useState("");
+
+  const handleRemoveImage = async () => {
+   const res = await imageRemove(imageKey);
+   if(res.success){
+    alert("One file is removed from server");
+    setImageUrl("");
+    setImageKey("");
+   }
+    console.log('removing image')
+  }
   return (
     <div>
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -10,6 +25,8 @@ const ImageUpload = () => {
         onClientUploadComplete={(res) => {
           // Do something with the response
           console.log("Files: ", res);
+          setImageUrl(res[0].ufsUrl);
+          setImageKey(res[0].key);
           alert("Upload Completed");
         }}
         onUploadError={(error: Error) => {
@@ -17,6 +34,21 @@ const ImageUpload = () => {
           alert(`ERROR! ${error.message}`);
         }}
       />
+
+
+      <div>
+        <h1>Image preview</h1>
+        {
+          imageUrl &&(
+            <div>
+              <Image src={imageUrl} alt="image" width={300} height={200} className="object-cover h-auto w-full " />
+            </div>
+          )
+        }
+        <button onClick={handleRemoveImage} className="text-white thext-sm font-bold px-2 rounded bg-red-500 " >
+          Remove Image
+        </button>
+      </div>
     </div>
     </div>
   )
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "9q6klkylv0.ufs.sh" ,
+
+      },
+    ]
+  }
 };
 
 export default nextConfig;
