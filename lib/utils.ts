b71ce586112bbee3d5647074b2b7638ebb7b6fd0
Shudipt0 <royshudipto318@gmail.com>
2025-05-28 18:17:08 +0600

    prisma migrate

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { PrismaClient } from './generated/prisma/client'
+
+export const prisma = new PrismaClient()
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
